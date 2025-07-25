1eef6d77198e75a7f7eecd13b63bf7d17f321f06
ajaian <ajaianc.a1011@gmail.com>
2025-07-25 21:03:27 +0530

    Auto-generated commit

---
+++ app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { ComponentsComponent } from './components/components.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, ComponentsComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent  implements OnInit  {
+  title = 'RainBow_Digital';
+  isTransparent:boolean = true;
+  constructor( private renderer : Renderer2) {}
+
+  ngOnInit() {
+        const navbarElement = document.querySelector('.navbar') as HTMLElement;
+    this.renderer.listen('window', 'scroll', (event) => {
+      const number = window.scrollY;
+      if (number > 150 || window.pageYOffset > 150) {
+          this.isTransparent=false;
+      } else {
+          this.isTransparent=true;
+      }
+    });
+  }
+}
